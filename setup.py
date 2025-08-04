96362b67c48bc1225c60d9c5de0a585f87b7bf2d
Florian Hahn <florian.hahn@stresing.de>
2025-08-04 12:59:13 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
 	name='stresing',
 	packages=find_packages(),
-	version='4.18.1',
-	description='Library to operate Stresing cameras',
+	version='4.18.2',
+	description='Module to operate Stresing cameras',
 	author='Florian Hahn',
 )
