2d1f29520a805157c7c7b0b432e0b51a3844385e
Florian Hahn <florian.hahn@stresing.de>
2025-11-19 18:21:51 +0100

    Update ESLSCDLL to 4.20.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
 	name='stresing',
 	packages=find_packages(),
-	version='4.18.5',
+	version='4.20.0',
 	description='Module to operate Stresing cameras',
 	author='Florian Hahn',
 )
