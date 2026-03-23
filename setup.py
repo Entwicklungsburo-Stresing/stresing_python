40535228e102f2f445c607fe1afc14fa1d301dbe
spalato <7659022+spalato@users.noreply.github.com>
2026-03-23 22:04:56 +0100

    Add pyproject.toml for packaging and remove setup.py
    Added `build` and `twine` as development dependencies.
    
    Tested build and local install successfully, both in editable and regular mode.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-	name='stresing',
-	packages=find_packages(),
-	version='4.20.1',
-	description='Module to operate Stresing cameras',
-	author='Florian Hahn',
-)
